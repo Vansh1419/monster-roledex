dea7ec85f6cfe59cd2fc9b3b3fa2a3ec0d30a1f9
Vansh1419 <vanshjain1419@gmail.com>
2022-07-02 14:36:18 +0530

    29. Monsters Rolodex - setState just completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from 'react';
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'vansh'
+      , adress: 'nanak nagar'
+      , roll: '66'
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            HI {this.state.name},Work at {this.state.adress}
+          </p>
+          <button onClick={() => {
+            this.setState({name:'sambhav',adress:'ZTM'})
+          }}>
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn hello
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
