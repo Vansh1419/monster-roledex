f20a0721789fb84afdc788fcf2b3bfbf3aa9a31b
Vansh1419 <vanshjain1419@gmail.com>
2022-07-02 23:10:24 +0530

    console.log(1); just completed

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ class App extends Component {
     this.state = {
       monsters: []
     }
-    console.log(1);
   }
   componentDidMount() {
-    console.log(3);
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {
@@ -25,9 +23,11 @@ class App extends Component {
       )
   }
   render() {
-    console.log(2);
     return (
       <div className="App">
+        <input type="search" className='search-box' placeholder='Search here' onChange={(event) => {
+          console.log(event.target.value)
+        }} />
         {
           this.state.monsters.map((monster) => {
             return <div key={monster.id}>
